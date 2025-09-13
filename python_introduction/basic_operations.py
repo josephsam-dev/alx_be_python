dc5bb66b56e813387dc0918a4fe11c4d0ba0c455
joseph segun <josephsegzy@gmail.com>
2025-09-13 10:00:35 +0100

    Add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+# Addition
+sum_result = number1 + number2
+print(f"Addition of {number1} and {number2} is {sum_result}")
+
+# Subtraction
+sub_result = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {sub_result}")
+
+# Multiplication
+mul_result = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {mul_result}")
