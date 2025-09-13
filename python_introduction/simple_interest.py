c3b317ed187a3e682c853cdcca697b2495433235
joseph segun <josephsegzy@gmail.com>
2025-09-13 10:09:24 +0100

    Add simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Variables
+principal = 1000
+rate = 0.05
+time = 3
+
+# Simple Interest formula: I = P * R * T
+interest = principal * rate * time
+
+# Output
+print(f"The simple interest is: {interest}")
